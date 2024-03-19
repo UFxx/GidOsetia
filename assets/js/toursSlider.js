3d131f8661a6fd96006688bb815ac397be63f9b2
UFxx <smileprost@gmail.com>
2024-03-19 21:25:14 +0300

    Fix tours slider (change method function)

--- assets/js/toursSlider.js
+++ assets/js/toursSlider.js
@@ -7,8 +7,28 @@ const sliderRightArrow = document.querySelector('.slider-arrow-right');
 const sliderLeft = document.querySelector('.slider-line-left');
 const sliderRight = document.querySelector('.slider-line-right');
 
+
+let i = 1;
+let sliderOffset = 0;
+let maxOffset = (sliderLineItems.length - 2) * 370;
+
 sliderMethodCheckbox.addEventListener('change', () => {
     if (sliderMethodCheckbox.checked) {
+        // Reset slider when checkbox checked
+        i = 1;
+        sliderOffset = 0;
+        sliderLineItems[i].classList.add('slider-line-item__active');
+        sliderItemTitle[i].classList.add('slider-item-title__active');
+        sliderLine.style.left = sliderOffset + 'px';
+        sliderLineItems.forEach(el => {
+            el.classList.remove('slider-line-item__active');
+        })
+        sliderItemTitle.forEach(el => {
+            el.classList.remove('slider-item-title__active')
+        })  
+        sliderLineItems[i].classList.add('slider-line-item__active');
+        sliderItemTitle[i].classList.add('slider-item-title__active');        
+
         sliderLeft.addEventListener('mouseover', () => {
             sliderOffset += 370;
             if (sliderOffset > maxOffset - (2 * 370)) {
@@ -62,10 +82,8 @@ sliderMethodCheckbox.addEventListener('change', () => {
     }
 })
 
-let sliderOffset = 0;
-let maxOffset = (sliderLineItems.length - 2) * 370;
 
-let i = 1;
+
 sliderLineItems[i].classList.add('slider-line-item__active');
 sliderItemTitle.forEach(el => {
     el.classList.remove('slider-item-title__active')
